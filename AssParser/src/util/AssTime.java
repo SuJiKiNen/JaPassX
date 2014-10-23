5bf8fe5f689952a7bd342b81f500e64c9790e83a
SuJiKiNen <mail.luishen@gmail.com>
2014-10-23 16:17:49 +0800

    add asstime constructor from ms,clean some blank line.

--- AssParser/src/util/AssTime.java
+++ AssParser/src/util/AssTime.java
@@ -9,18 +9,20 @@ public class AssTime {
 	public int hs;
 	public AssTime(String format) {
 		// to do add check
-		
-		
-		
 		format = format.replace(".", ":");
 		Pattern p = Pattern.compile(":");
-
 		String[] items = p.split(format);
 		h = Integer.parseInt(items[0]);
 		m = Integer.parseInt(items[1]);
 		s = Integer.parseInt(items[2]);
 		hs = Integer.parseInt(items[3]);
 		
+	}
+	public AssTime(int ms) {
+		int h  = (ms / 3600000) % 10;
+		int m  = (ms % 3600000) / 60000;
+		int s  = (ms % 60000) / 1000;
+		int hs = (ms % 1000) / 10;
 	}
 	public int toMillis(){
 		int ms = hs * 10 + s * 1000 + m * 60000 + h * 3600000;

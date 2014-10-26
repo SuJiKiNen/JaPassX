3d9fb39a3c3429c6725a9bd1926aea0240e2b859
SuJiKiNen <mail.luishen@gmail.com>
2014-10-26 17:27:03 +0800

    try space test with fullwidth

--- AssParser/src/ass/test/FontExtentTest.java
+++ AssParser/src/ass/test/FontExtentTest.java
@@ -10,11 +10,12 @@ public class FontExtentTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Style style = new Style("Style: Default,·ÂËÎ,30,&H0BFFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,2,2,10,10,10,1");
-		Line line = new Line("Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,fx,30 30.015625ô´");
+		Style style = new Style("Style: Default2,ä»¿å®‹,30,&H0BFFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,0,0,5,0,0,10,1");
+		Line line = new Line("Dialogue: 0,0:00:00.00,0:00:05.00,Default2,,0,0,0,,èˆ¸123");
 		
 		// size infomation seems wrong
-		TextExtents textExtents = new TextExtents("ô´", style);
+		TextExtents textExtents = new TextExtents("ã€€12", style);
+		System.out.println(line.text);
 		System.out.println(style.fontSize);
 		System.out.println(textExtents.getHeight());
 		System.out.println(textExtents.getWidth());

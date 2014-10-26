80755d5a88c9bb2807e81bfefe7eeda0ee1d79d6
SuJiKiNen <mail.luishen@gmail.com>
2014-10-26 09:05:06 +0800

    rename clean to strip

--- AssParser/src/util/AssTag.java
+++ AssParser/src/util/AssTag.java
@@ -4,13 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AssTag {
-	public static String clean(String text){
+	public static String strip(String text){
 		Pattern pattern = Pattern.compile("\\{[^\\{]*\\}");
 		Matcher matcher = pattern.matcher(text);
 		return matcher.replaceAll("");
 	}
 	public static void main(String [] args) {
 		String s="123{\\kfsc}12";
-		System.out.println(AssTag.clean(s));
+		System.out.println(AssTag.strip(s));
 	}
 }

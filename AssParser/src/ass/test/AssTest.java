def4325e1f2b29357fc94c108833d033cbf31d49
SuJiKiNen <mail.luishen@gmail.com>
2014-10-30 13:05:51 +0800

    Change parseFile to static function,enable framteRate to variable arguments

--- AssParser/src/ass/test/AssTest.java
+++ AssParser/src/ass/test/AssTest.java
@@ -10,10 +10,8 @@ public class AssTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Line line = new Line("Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,,123ô´");
+		Line line = new Line("Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,,123ï¿½ï¿½");
 		System.out.print(line.kText);
-		AssParser assParser = new AssParser(30);
-		System.out.print(assParser.getFrameRate());
 	}
 	
 }
